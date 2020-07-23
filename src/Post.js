f070ec9f012b8ae8ccb4e897f4c1f4133059b79e
sanddwich <64834314+sanddwich@users.noreply.github.com>
2020-07-23 13:15:07 +0400

    23-07-2020_1

---
+++ src/Post.js
@@ -0,0 +1,13 @@
+class Post {
+  constructor(title) {
+    this.title = title;
+    this.date = new Date();
+  }
+
+  toString() {
+    JSON.stringify({
+      title: this.title,
+      date: this.date.toJSON(),
+    })
+  }
+}
